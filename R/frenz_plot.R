0e4c02244351c74c917a9dd6f285c5a1d1910a1a
marette <20315546@sun.ac.za>
2019-09-13 14:26:40 +0200

    extended plot function

--- R/frenz_plot.R
+++ R/frenz_plot.R
@@ -1,15 +1,17 @@
 
-#' Quickly plotting a criterion network and the respondents accuracy.
+#' Quickly plotting a criterion network and optionally the respondents accuracy-scores.
 #'
 #' @description
 #' This function can be used to quickly plot a summary of the \link[Rfrenz:get_criterion]{get_criterion()}and \link[Rfrenz:get_accuracy]{get_accuracy()}functions in one place.
+#' The function allows you to plot the criterion alone or with an accuracy-score summary in the form of a boxplot. If you want to plot the criterion alone , you only have to pass a criterion-matrix to the function.
 #'
 #' @usage
-#' frenz_plot(crit, accuracy_df,criterion="RLAS")
+#' frenz_plot(crit,criterion="Title Here",accuracy_df)
 #'
-#' @param crit criterion network returned after \link[Rfrenz:get_criterion]{get_criterion()} is run. the crit is used to plot a criterion network.
-#' @param accuracy_df accuracy score dataframe that is return after \link[Rfrenz:sget_accuracy]{get_accuracy()}from Rfrenze is run. The accruacy_df is used to plot a boxplot summarising the data
-#' @param criterion default="RLAS". the criterion name of the criterion being past in crit. is used as a titile for the graph.
+#' @param crit criterion network returned after \link[Rfrenz:get_criterion]{get_criterion()} is run. The crit is used to plot a criterion network.
+#' @param criterion Default="Title Here". The title of the criterionnetwork being plotted.
+#' @param accuracy_df Default = FALSE. If you have an accuracy dataframe from the \link[Rfrenz:sget_accuracy]{get_accuracy()} function you can past it to the parameter here. A boxplot is plotted of for the various accuracy scores.
+#'
 #'
 #' @import igraph
 #' @import dplyr
@@ -23,7 +25,7 @@
 #' @import gridExtra
 #'
 #'@export
-frenz_plot <- function(crit, accuracy_df,criterion="RLAS"){
+frenz_plot <- function(crit, criterion="Criterion Title Here", accuracy_df=FALSE){
 
   crit_net<- ggraph(as_tbl_graph(crit), layout = 'auto') +
     geom_edge_fan(arrow = arrow(length = unit(0.08, "inches")),start_cap = circle(4,'mm') , end_cap = circle(4, 'mm')) +
@@ -32,22 +34,26 @@ frenz_plot <- function(crit, accuracy_df,criterion="RLAS"){
     ggtitle(criterion)+
     theme_graph()
 
+  if(accuracy_df==TRUE){
+    box <- ggplot(accuracy_df, aes(y=Score))+
+      geom_boxplot()+
+      coord_flip()+
+      ylab("Accuracy Scores of Respondents")+
+      theme_minimal()+
+      theme(axis.title.y=element_blank(),
+            axis.text.y=element_blank(),
+            axis.ticks.y=element_blank(),
+      )
 
-  box <- ggplot(accuracy_df, aes(y=Score))+
-    geom_boxplot()+
-    coord_flip()+
-    ylab("Accuracy Scores of Respondents")+
-    theme_minimal()+
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank(),
-    )
+    lay <- rbind(c(NA,1,1,1,1,NA),
+                 c(NA,1,1,1,1,NA),
+                 c(NA,1,1,1,1,NA),
+                 c(2,2,2,2,2,2))
 
-  lay <- rbind(c(NA,1,1,1,1,NA),
-               c(NA,1,1,1,1,NA),
-               c(NA,1,1,1,1,NA),
-               c(2,2,2,2,2,2))
+    g<-grid.arrange(crit_net,box, layout_matrix=lay)
 
-  g<-grid.arrange(crit_net,box, layout_matrix=lay)
+  }else{
+    return(crit_net)
+  }
 
 }
